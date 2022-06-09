abde51bc019f4d7d86505b49c19b5db22b4b3a61
Gabi Chiosa <gabi@bittreatsoftware.com>
2022-06-09 19:02:44 +0300

    - updated package file

--- Package.swift
+++ Package.swift
@@ -4,16 +4,17 @@
 import PackageDescription
 
 let package = Package(
-    name: "PagoNetworkSDK",
+    name: "PagoApiClient",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "PagoNetworkSDK",
+            name: "PagoApiClient",
             targets: ["PagoNetworkSPM"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/PagoApp/ios-core-spm", branch: "main"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -24,7 +25,8 @@ let package = Package(
         .target(
             name: "PagoNetworkSPM",
             dependencies: [
-                .target(name: "PagoApiClient")
+                .target(name: "PagoApiClient"),
+                .product(name: "PagoCoreSDK", package: "ios-core-spm")
             ]),
         .testTarget(
             name: "PagoNetworkSPMTests",
