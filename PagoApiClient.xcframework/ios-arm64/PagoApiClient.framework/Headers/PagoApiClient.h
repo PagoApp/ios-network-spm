16e2c0e98de560d7379823d0494742d81f30e005
Gabi Chiosa <gabi@bittreatsoftware.com>
2022-06-14 17:41:02 +0300

    added sdk

---
+++ PagoApiClient.xcframework/ios-arm64/PagoApiClient.framework/Headers/PagoApiClient.h
@@ -0,0 +1,18 @@
+//
+//  PagoApiClient.h
+//  PagoApiClient
+//
+//  Created by Andrei Chirita on 25.05.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PagoApiClient.
+FOUNDATION_EXPORT double PagoApiClientVersionNumber;
+
+//! Project version string for PagoApiClient.
+FOUNDATION_EXPORT const unsigned char PagoApiClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PagoApiClient/PublicHeader.h>
+
+
